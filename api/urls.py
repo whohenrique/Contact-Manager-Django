b0e9a55b1b7ec4a8b0c0afe9783c0a570419b50d
Henrique <heduardocr7@gmail.com>
2024-03-14 13:17:05 -0300

    Finished

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from rest_framework.documentation import include_docs_urls
 from .api.views import CreateContactsViewSet
 
 router = routers.DefaultRouter()
-router.register(r'contacts', CreateContactsViewSet, "contacts")
+router.register(r'contacts', CreateContactsViewSet, basename="contacts")
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
     path('docs/', include_docs_urls(title="Contacts API")),
 ]
+urlpatterns += router.urls
